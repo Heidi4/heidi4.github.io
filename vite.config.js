5243e8f82ebd8f4e86980c599d4592603504377e
Heidi4 <erickkaloki28@gmail.com>
2024-03-18 16:55:59 +0300

    modified the base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/heidi4.github.io/"
-})
+  base: "https://heidi4.github.io/",
+});
